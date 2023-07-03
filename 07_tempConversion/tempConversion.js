249c6e392a92e89f5ed4728e45d8d1b1f7ade053
Savannaha <savwiley@yahoo.com>
2023-07-02 23:05:13 -0500

    finish tempConversion tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  //(32°F − 32) × 5/9 = 0°C
+  let celsius = (F - 32) * 5/9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  //(0°C × 9/5) + 32 = 32°F
+  let fahrenheit = (C * 9/5) + 32;
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
