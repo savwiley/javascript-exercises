55a8a967a6fff7e709afacf61470c828c8321126
Savannaha <savwiley@yahoo.com>
2023-07-02 23:03:42 -0500

    finish repeatString tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let arr = [];
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      arr.push(string);
+    }
+    return arr.join('');
+  }
 };
 
 // Do not edit below this line
