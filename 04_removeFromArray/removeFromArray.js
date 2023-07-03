fde621147fec49f1da0a86e27c6800d427a21316
Savannaha <savwiley@yahoo.com>
2023-07-02 23:04:20 -0500

    finish removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  let array = arr;
+  for (arg of args) {
+    if (array.includes(arg)) {
+      let index = array.indexOf(arg);
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
