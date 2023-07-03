8f111069f7f59d9059ca30ea6d82c9498e03ed1e
Savannaha <savwiley@yahoo.com>
2023-07-02 23:04:33 -0500

    finish sumAll tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  let sum = 0;
+  let min = Math.min(...[a, b]);
+  let max = Math.max(...[a, b]);
+  if (typeof(a) != 'number' || typeof(b) != 'number' || (a || b) < 0) {
+    return "ERROR"
+  } else {
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
