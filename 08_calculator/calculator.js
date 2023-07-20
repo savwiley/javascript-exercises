4263ada1c32497cba0110f17284f43a02a476e5f
Savannaha <savwiley@yahoo.com>
2023-07-19 19:23:42 -0500

    finish calculator lesson

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let initial = 0;
+	return arr.reduce((accumulator, current) => accumulator + current, initial);
 };
 
-const multiply = function() {
-
+const multiply = function(...numbs) {
+  let total;
+  for (let i = 0; i < numbs.length; i++) {
+    !total ? total = numbs[i] : total *= numbs[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(numb) {
+	let newNumb;
+  if (numb === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= numb; i++) {
+      !newNumb ? newNumb = i : newNumb *= i;
+    }
+    return newNumb;
+  }
 };
 
 // Do not edit below this line
