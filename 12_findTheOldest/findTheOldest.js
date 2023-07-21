e34ce76bed30a73f588010381261b2804d43fe39
Savannaha <savwiley@yahoo.com>
2023-07-21 14:03:47 -0500

    finish findTheOldest lesson

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let newArr = [];
+  arr.forEach(person => {
+    let death = person.yearOfDeath;
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    newArr.push(death - person.yearOfBirth);
+  });
+  let index = newArr.indexOf(Math.max(...newArr));
+  return arr[index];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+//npm test findTheOldest.spec.js
