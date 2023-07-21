4433ffc2803485e9a067c747495f4644385165af
Savannaha <savwiley@yahoo.com>
2023-07-20 21:48:28 -0500

    finish palindromes lesson

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const caps = str.toUpperCase().replace(/[^A-Z0-9]/g, "");
+  return caps === caps.split('').reverse().join('');
 };
 
 // Do not edit below this line
